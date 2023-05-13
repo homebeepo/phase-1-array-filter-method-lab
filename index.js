0b35eee55f6343a1254a8afd86d02a16832e0dde
caseyclinch <clinchcasey@gmail.com>
2023-05-12 19:02:29 -0700

    Completed Project

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function findMatching(anArray, toll) {
+    return anArray.filter((i) => i.toUpperCase() === toll.toUpperCase())}
+
+function fuzzyMatch(anArray, toll) {
+    return anArray.filter((i) => i.startsWith(toll))}
+
+function matchName(anArray, toll) {
+    return anArray.filter((i) => i.name === toll)}
